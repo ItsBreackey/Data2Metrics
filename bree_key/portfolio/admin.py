0af6323134c95ad6b7e148ea07006a8ee9f1e57f
breackey <brendaawino41@gmail.com>
2026-04-11 01:34:34 +0300

    Migrate from Django to Node.js (Express + EJS)
    - Remove entire Django/Python backend (bree_key/, requirements.txt)
    - Add Express server with EJS templating
    - Single-page site: hero, services, process, portfolio, testimonials, blog, contact
    - Glassmorphism dark theme (lavender #B57EDC / pink #F472B6)
    - Auto-sliding carousels, scroll animations, responsive navbar
    - SEO meta tags, dot navigation, social links, subscribe form

--- bree_key/portfolio/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Project, BlogPost, Testimonial, ContactMessage, Technology
-
-admin.site.register(Technology)
-admin.site.register(Project)
-admin.site.register(BlogPost)
-admin.site.register(Testimonial)
-admin.site.register(ContactMessage)
